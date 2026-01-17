471fd76ba7ae155c8d12a296718f74eff99c19f2
Aashir <48567707+Aashir5678@users.noreply.github.com>
2026-01-17 12:56:38 -0500

    sending images over tcp not working

--- network/client.py
+++ network/client.py
@@ -1,70 +1,136 @@
 
-# Author: Aashir Alam (fixed & simplified)
 
 import socket
 import threading
+import pickle
+from PIL import Image
+import io
+import cv2
+
+from time import sleep
 
 PORT = 5050
 HEADER = 128
+IMG_HEADER = 256
 FORMAT = "utf-8"
+IMG_MSG = "!IMG"
+CMD_MSG = "!CMD"
+
 
 class Client:
-    def __init__(self, username, server_ip, server_pass=""):
-        self.username = username
-        self.server_ip = server_ip
-        self.server_pass = server_pass
-        self.connected = True
+	def __init__(self, server_ip):
+		self.server_ip = server_ip
+		self.connected = True
 
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def send(self, msg):
-        message = msg.encode(FORMAT)
-        msg_len = len(message)
-        header = str(msg_len).encode(FORMAT)
-        header += b" " * (HEADER - len(header))
-        self.sock.sendall(header)
-        self.sock.sendall(message)
+	def send(self, msg):
+		self.sock.sendall(CMD_MSG.encode(FORMAT))
 
-    def receive(self):
-        header = self.sock.recv(HEADER).decode(FORMAT)
-        if not header:
-            return None
-        length = int(header.strip())
-        return self.sock.recv(length).decode(FORMAT)
+		message = msg.encode(FORMAT)
+		msg_len = len(message)
+		header = str(msg_len).encode(FORMAT)
+		header += b" " * (HEADER - len(header))
+		self.sock.sendall(header)
+		self.sock.sendall(message)
 
-    def listen(self):
-        while self.connected:
-            try:
-                msg = self.receive()
-                if msg:
-                    print(msg)
-            except:
-                break
 
-    def start(self):
-        self.sock.connect((self.server_ip, PORT))
-        print("[CONNECTED]")
+	def send_image(self, img):
+		size = len(img)
 
-        self.send(self.username)
-        self.send(self.server_pass)
+		print ("starting send img")
 
-        thread = threading.Thread(target=self.listen, daemon=True)
-        thread.start()
+		self.sock.sendall(IMG_MSG.encode(FORMAT))
 
-        while True:
-            msg = input()
-            if msg.lower() == "q":
-                break
-            self.send(msg)
+		byte_padding = b" " * (HEADER - len(str(size).encode(FORMAT)))
+		self.sock.sendall(str(size).encode(FORMAT) + byte_padding)
 
-        self.connected = False
-        self.sock.close()
+		self.sock.sendall(img)
+		
+		# if size < IMG_HEADER:
+		# 	self.sock.sendall(img + b" " * (IMG_HEADER - size))
+
+		# else:
+
+		# 	start_index = 0
+
+		# 	for img_index in range(IMG_HEADER, size, IMG_HEADER):
+		# 		self.sock.sendall(img[start_index:img_index])
+
+		# 		# print(str(start_index), ", " + str(img_index))
+		# 		start_index = img_index
+
+
+		# 	# send remaining image data if size isn't evenly divisible by header
+		# 	if size % IMG_HEADER != 0:
+		# 		padding_bytes = b" " * ((size % IMG_HEADER) - (size - IMG_HEADER))
+		# 		self.sock.sendall(img[(size - IMG_HEADER)::] + padding_bytes)
+		# 		# print(str(size - IMG_HEADER) + ", " + str(size - 1))
+		
+
+		print ("done")
+
+
+	def receive(self):
+		header = self.sock.recv(HEADER).decode(FORMAT)
+		if not header:
+			return None
+		length = int(header.strip())
+		return self.sock.recv(length).decode(FORMAT)
+
+	def listen(self):
+		while self.connected:
+			try:
+				msg = self.receive()
+
+			except Exception as e:
+				print (e)
+				break
+
+	def start(self):
+		self.sock.connect((self.server_ip, PORT))
+		print("[CONNECTED]")
+
+		thread = threading.Thread(target=self.listen, daemon=True)
+		thread.start()
+
+		# with open("flower.jpg", "rb") as f:
+		# 	img = f.read()
+
+
+		cap = cv2.VideoCapture(0)
+
+		while True:
+			ret, frame = cap.read()
+
+			if ret:
+
+				self.send_image(cv2.imencode('.jpg', frame)[1].tobytes())
+
+			# msg = input(f"{self.username}: ")
+
+			# self.send_image(("b" * 550).encode("utf-8"))
+
+			# self.send_image(img)
+			# self.send(msg)
+
+
+			# if msg.lower() == "q":
+			# 	break
+			# self.send(msg)
+
+			sleep(0.5)
+
+		cap.release()
+		self.connected = False
+		self.sock.close()
 
 
 if __name__ == "__main__":
-    username = input("Username: ")
-    server_ip = input("Server IP address: ")
-    server_pass = input("Server password (if any): ")
 
-    client = Client(username, server_ip, server_pass)
-    client.start()
+	# username = input("Username: ")
+	server_ip = input("Server IP address: ")
+	# server_pass = input("Server password (if any): ")
+
+	client = Client(server_ip)
+	client.start()
--- network/server.py
+++ network/server.py
@@ -1,5 +1,4 @@
 
-# Author: Aashir Alam (fixed & simplified)
 
 """
 SERVER HOST MUST DISABLE HOTSPOT FOR CLIENTS TO JOIN
@@ -9,10 +8,19 @@ SERVER HOST MUST DISABLE HOTSPOT FOR CLIENTS TO JOIN
 
 import socket
 import threading
+# from PIL import Image
+import cv2
+import io
+import numpy as np
 
 PORT = 5050
 HEADER = 128
+IMG_HEADER = 256 # 128
 FORMAT = "utf-8"
+
+
+IMG_MSG = "!IMG"
+CMD_MSG = "!CMD"
 
 DISCONNECT_MSG = "!DISCONNECT"
 WRONG_PASS_MSG = "!WRONGPASSWORD"
@@ -22,16 +30,17 @@ class Server:
         self.SERVER = "0.0.0.0"
         self.ADDR = (self.SERVER, PORT)
         self.server_password = server_pass
+        self.img_buff = None
 
-        self.clients = {}
+        # self.clients = {}
         self.messages = []
-        self.lock = threading.Lock()
 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def start(self):
         self.server.bind(self.ADDR)
         self.server.listen()
+
         print(f"[SERVER] Listening on port {PORT}")
 
         while True:
@@ -49,57 +58,143 @@ class Server:
         conn.sendall(message)
 
     def receive(self, conn):
-        header = conn.recv(HEADER).decode(FORMAT)
-        if not header:
+        try:
+            header = conn.recv(HEADER).decode(FORMAT)
+
+        except UnicodeDecodeError as e:
             return None
-        length = int(header.strip())
-        return conn.recv(length).decode(FORMAT)
+
+        if header == IMG_MSG:
+            return self.receive_img(conn)
+
+
+        elif header == CMD_MSG:
+            if not header:
+                return None
+
+            length = int(conn.recv(HEADER).decode(FORMAT).strip())
+            msg = conn.recv(length).decode(FORMAT)
+
+            self.lock.acquire()
+            self.messages.append(msg)
+            self.lock.release()
+
+            return msg
 
 
     
     def handle_client(self, conn, addr):
-        try:
-            username = self.receive(conn)
-            password = self.receive(conn)
+        # try:
+        get_frames_thread = threading.Thread(target=self.get_frames, args=(conn,))
 
-            if self.server_password and password != self.server_password:
-                self.send(conn, WRONG_PASS_MSG)
-                conn.close()
-                return
 
-            with self.lock:
-                self.clients[username] = conn
-                join_msg = f"{username} has joined."
-                print(join_msg)
-                self.broadcast(join_msg)
+        get_frames_thread.start()
 
-            while True:
-                msg = self.receive(conn)
-                if msg is None:
-                    break
+        while True:
+            self.display_img()
+            
+            # msg = self.receive(conn)
 
+            # if msg is None:
+            #     break
+
+            # elif msg == IMAGE_MSG:
+            #     # print("received image")
+            #     self.receive_img(conn)
+
+
+            # full_msg = f"{username}: {msg}"username
+            # print(full_msg)
+            # self.broadcast(full_msg)
+
+        # except Exception as e:
+        # 	print(e)
+
+        # finally:
+
+        # 	# Delete client if error
+
+        #     with self.lock:
+        #         if username in self.clients:
+        #             del self.clients[username]
+
+        #     conn.close()
+        #     leave_msg = f"{username} has disconnected."
+        #     print(leave_msg)
+        #     self.broadcast(leave_msg)
+
+    def get_frames(self, conn):
+        while True:
+            msg = self.receive(conn)
+
+            if msg is None:
+                break
+
+            elif msg == IMG_MSG:
+                self.receive_img(conn)
+
+            elif msg == CMD_MSG:
                 full_msg = f"{username}: {msg}"
                 print(full_msg)
-                self.broadcast(full_msg)
 
-        except:
-            pass
 
-        finally:
-            with self.lock:
-                if username in self.clients:
-                    del self.clients[username]
-            conn.close()
-            leave_msg = f"{username} has disconnected."
-            print(leave_msg)
-            self.broadcast(leave_msg)
 
-    def broadcast(self, msg):
-        for conn in self.clients.values():
-            try:
-                self.send(conn, msg)
-            except:
-                pass
+
+    def receive_img(self, conn):
+        print ("starting to read img")
+        # buff = bytearray()
+
+        size = int(conn.recv(HEADER).decode(FORMAT))
+        self.img_buff = conn.recv(size)
+
+        # for img_index in range(0, size, IMG_HEADER):
+        #     buff.extend(conn.recv(IMG_HEADER))
+
+
+
+        # # buffer remaining image
+        # if size % IMG_HEADER != 0:
+        #     buff.extend(conn.recv(IMG_HEADER))
+
+
+        # # print (f"received {str(len(img))} size img")
+
+        # with open("flower.jpg", "wb") as f:
+        #     f.write(bytes(buff))
+
+        # img = Image.open(io.BytesIO(bytes(buff)))
+
+        # img.show()
+
+        with open("flower.jpg", "wb") as f:
+            f.write(self.img_buff)
+
+
+        
+
+        # with open("")
+
+        return self.img_buff
+
+
+    def display_img(self):
+        # if self.img_buff == None:
+        #     return
+
+        with open("flower.jpg", "rb") as f:
+            self.img_buff = f.read()
+
+        if self.img_buff == None or not self.img_buff:
+            return
+
+        frame = cv2.imdecode(np.frombuffer(self.img_buff, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+        cv2.imshow("webcam", frame)
+        cv2.waitKey(1)
+        # print ("showing")
+
+
+
 
 
 if __name__ == "__main__":
